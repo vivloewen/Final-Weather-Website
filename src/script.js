d2523f20de491d5fc6e37b5efc9121384d6bdbc9
Viv <vivienneloewen@googlemail.com>
2022-03-10 09:15:00 +0100

    Inluded API forecast to go live for each day

--- src/script.js
+++ src/script.js
@@ -79,24 +79,38 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-function displayForecast() {
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#weather-forecast");
   let forecastHTML = "";
   forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col forecastDays">
-        <span class="dayOne">${day}</span>
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6 && index >= 1) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col forecastDays">
+        <span class="dayOne">${formatDay(forecastDay.dt)}</span>
         <br />
         <span class="icon" id="iconOne">
-        <img src="img/rain.png" alt="Sunny" id="forecastIcon" />
+        <img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="Sunny" id="forecastIcon" />
         </span>
         <br />
-        <span class="max">3°</span> <span class="min">1°</span>
+        <span class="max">${Math.round(
+          forecastDay.temp.max
+        )}°</span> <span class="min">${Math.round(forecastDay.temp.min)}°</span>
     </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
